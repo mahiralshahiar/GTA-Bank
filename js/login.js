62a518904304131c27635cf46a36f39c571a511f
mahiralshahiar <mrmahiralshariar9@gmail.com>
2023-08-18 23:49:16 +0600

    first

---
+++ js/login.js
@@ -0,0 +1,8 @@
+document.getElementById('btn-submit').addEventListener('click', function () {
+    // .value add korte hobe input field er somoy
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    // ekhane password paua jabe
+    const passwordField = document.getElementById('user-password')
+
+})
